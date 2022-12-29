1d8ef6f7812eef728d054e6ce2ad616eb0fb627d
Asya19 <asya.kisko@yandex.ru>
2022-12-29 23:46:10 +0300

    feat: add services container

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,8 +1,49 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
 
 namespace Bookinist
 {
     public partial class App : Application
     {
+        private static IHost __Host; // use pattern singleton
+
+        // Первое обращение к этому публичному статическому свойству Host
+        // заставит этот Host создаться
+        // с помощью метода CreateHostBuilder
+
+        // последующие обращения будут переадреованны к полю __Host,
+        // которое будет заполнено при первом обращение к нему
+        public static IHost Host => __Host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        // создаем возможность доступа к сервисам посредствам хоста
+        // если обращение к сервисам будет раньше, чем заплонировалось, то
+        // это все равно приведет к создаю хоста
+        public static IServiceProvider Service => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // Запуск хоста
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        // Остановка хоста
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
+
---
+++ Bookinist/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        // если этого не сделать придется создавать отдельный класс
+        // чтобы формировать контекст базы данных
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices);
+    }
+}
