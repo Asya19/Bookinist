0fa5060edeabc843bd6e00acfc35c044df12f05d
Asya19 <asya.kisko@yandex.ru>
2022-12-29 23:12:24 +0300

    feat: add MainWindowViewModel and ViewmodelsLocator

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MathCore.WPF.ViewModels;
+
+namespace Bookinist.ViewsModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Title : string = Заголовок
+        private string _Title = "Главное окно программы";
+
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+        #endregion
+    }
+}
